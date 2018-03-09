c0f9b195aaebc8d84e08d29a699b9f5cddae4b7d
Trang Le T <tranglet.@MBA0127s-MacBook-Air.local>
2018-03-09 16:26:13 +0700

    config lombok

---
+++ app/src/main/java/com/example/tranglet/configannotation/model/Demo.java
@@ -0,0 +1,12 @@
+package com.example.tranglet.configannotation.model;
+
+import lombok.Data;
+
+/**
+ * @author tranglet.
+ * @create by tranglet. on 3/9/18.
+ */
+@Data
+public class Demo {
+    private String name;
+}
